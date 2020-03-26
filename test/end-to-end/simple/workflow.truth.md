cec26b70ba157cc6c80e4108419f2e6a776d103a
Andrew Frantz <andrew.frantz@stjude.org>
2020-03-26 14:10:03 -0500

    refactor: change newlines and update test

--- test/end-to-end/simple/workflow.truth.md
+++ test/end-to-end/simple/workflow.truth.md
@@ -1,6 +1,10 @@
-# Workflow
+# hello_world
+
 
 ## Inputs
-
+#### Required
   * `name` (String, **required**): Name of the individual to greet.
   * `message` (String, **required**): Message to greet `name` with.
+
+## Outputs
+  * `greet.response` (String)
--- wdldoc/templates/markdown.py
+++ wdldoc/templates/markdown.py
@@ -6,11 +6,11 @@ import WDL as wdl
 
 class MarkDownDoc:
     def __init__(self, title: str) -> None:
-        self.title = f"# {title}\n"
+        self.title = f"# {title}"
         self.front_matter = ""
-        self.inputs = "## Inputs\n\n"
-        self.outputs = "## Outputs\n\n"
-        self.todo = "## To Do\n\n"
+        self.inputs = "\n## Inputs"
+        self.outputs = "\n## Outputs"
+        self.todo = "\n## To Do"
 
     def generate_frontmatter(self, source_text: str) -> None:
         comments = re.findall("^##.*", source_text, re.MULTILINE)
@@ -23,38 +23,35 @@ class MarkDownDoc:
         parameter_metadata: Dict[str, Any],
     ) -> None:
         if inputs["required"].items():
-            self.inputs += "#### Required\n"
+            self.inputs += "\n#### Required"
         for name, value in inputs["required"].items():
-            self.inputs += f"  * `{name}` ({value.type}, **required**)"
+            self.inputs += f"\n  * `{name}` ({value.type}, **required**)"
             description = parameter_metadata.get(value.name)
             if description:
                 self.inputs += ": {}".format(description)
-            self.inputs += "\n"
 
         if inputs["optional"].items():
-            self.inputs += "#### Optional\n"
+            self.inputs += "\n#### Optional"
         for name, value in inputs["optional"].items():
-            self.inputs += f"  * `{name}` ({value.type})"
+            self.inputs += f"\n  * `{name}` ({value.type})"
             description = parameter_metadata.get(value.name)
             if description:
                 self.inputs += ": {}".format(description)
-            self.inputs += "\n"
 
         if inputs["default"].items():
-            self.inputs += "#### Defaults\n"
+            self.inputs += "\n#### Defaults"
         for name, value in inputs["default"].items():
-            self.inputs += f"  * `{name}` ({value.type}, default={value.expr})"
+            self.inputs += f"\n  * `{name}` ({value.type}, default={value.expr})"
             description = parameter_metadata.get(value.name)
             if description:
                 self.inputs += ": {}".format(description)
-            self.inputs += "\n"
 
-        if self.inputs == "## Inputs\n\n":
-            self.inputs += "None\n"
+        if self.inputs == "## Inputs":
+            self.inputs += "\nNone"
 
     def generate_outputs(self, outputs: wdl.Env.Bindings) -> None:
         for output in outputs:
-            self.outputs += f"  * `{output.name}` ({output.value})\n"
+            self.outputs += f"\n  * `{output.name}` ({output.value})"
 
-        if self.outputs == "## Outputs\n\n":
-            self.outputs += "None\n"
+        if self.outputs == "## Outputs":
+            self.outputs += "\nNone"
